174214b331d9ee13ec3ed91882118406f56b45fd
Aminebennani1 <147489402+Aminebennani1@users.noreply.github.com>
2024-09-10 12:09:13 +0100

    Update exercice1.js

--- exercice1/exercice1.js
+++ exercice1/exercice1.js
@@ -2,13 +2,14 @@ let num = [5, 0, 6, 11];
 let TE;
 for (let i = 0; i < num.length; i++) {
     for (let e = i + 1; e < num.length; e++) {
-        // cette boucle pour comparer les nombres un par un 
-        if (num[i] < num[e]) {
+      
+
+       if (num[i] > num[e]) {
             TE = num[i]
             num[i] = num[e]
             num[e] = TE
         }
-    }
+    } 
 }
 
 console.log(num);
